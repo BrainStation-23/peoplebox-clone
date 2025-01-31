468665bc9c9f3922e354d6002d25d2ce91fa6d4d
Tanimul Haque Khan <tazimtazim2012@gmail.com>
2025-01-31 23:34:34 +0600

    Update GroupedBarChart.tsx

--- src/pages/admin/surveys/campaigns/[id]/components/ReportsTab/charts/GroupedBarChart.tsx
+++ src/pages/admin/surveys/campaigns/[id]/components/ReportsTab/charts/GroupedBarChart.tsx
@@ -19,7 +19,7 @@ export function GroupedBarChart({
 }: GroupedBarChartProps) {
   return (
     <ChartContainer config={{}}>
-      <ResponsiveContainer width="100%" height={height}>
+      <ResponsiveContainer width="100%" height="100%">
         <BarChart 
           data={data} 
           margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
